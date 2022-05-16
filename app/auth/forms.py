12b9f4ec9d9f446013320499c92c16e1e4743d2c
VONETTA <thevonetteway@gmail.com>
2022-05-16 22:48:41 +0300

    added imports to __init__.py

---
+++ app/auth/forms.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import ValidationError, StringField,PasswordField,SubmitField,BooleanField
+from wtforms.validators import DataRequired,Email,EqualTo
+from app.models import User
+
+class LoginForm(FlaskForm):
+    username = StringField('Username',validators= [DataRequired()])
+    password = PasswordField('Password',validators= [DataRequired()])
+    remember = BooleanField('Remember Me!')
+    submit = SubmitField('Login')
+
+class RegForm(FlaskForm):
+    email = StringField('Your Email Address', validators= [DataRequired(),Email()])
+    username = StringField('Enter Your Username', validators= [DataRequired()])
+    password = PasswordField('Password',validators =  [DataRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators =  [DataRequired()])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email = data_field.data).first():
+            raise ValidationError("The Email has already been taken!")
+    
+    def validate_username(self, data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError("The username has already been taken")
