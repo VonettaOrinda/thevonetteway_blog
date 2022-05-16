cc093768fd7d0f0e8536cb6fb033483feb4ce652
VONETTA <thevonetteway@gmail.com>
2022-05-16 22:00:27 +0300

    added imports to config.py

---
+++ config.py
@@ -0,0 +1,46 @@
+import os
+class Config:
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+  
+    # SQLALCHEMY_DATABASE_URI = ''
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'Pitch-Perfect'
+    SENDER_EMAIL = 'vonetta.orinda@student.moringaschool.com'
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    @staticmethod
+    def init_app(app):
+        pass
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    uri = os.getenv('DATABASE_URL')
+    if uri and uri.startswith('postgres://'):
+        uri = uri.replace('postgres://', 'postgresql://', 1)
+        SQLALCHEMY_DATABASE_URI=uri
+class TestConfig(Config):
+   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://vonetta:vonetta@localhost/pitchp'
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://vonetta:vonetta@localhost/pitchp'
+    DEBUG = True
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
